e663a530141a32d5c93034154eeab733159d8aaf
lvh0301 <levanhieu0301@gmail.com>
2026-07-30 23:13:21 +0700

    Tính năng Xóa folder

--- routes/upload.route.ts
+++ routes/upload.route.ts
@@ -10,4 +10,11 @@ router.patch('/delete-file',upload.none(), uploadController.deleteFileName)
 router.post('/folder/create',upload.none(), uploadController.folderCreate)
 router.get('/folder/list',upload.none(), uploadController.folderList)
 
+router.patch(
+  '/folder/delete', 
+  upload.none(), 
+  uploadController.deleteFolder
+);
+
+
 export default router;
